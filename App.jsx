8fc538a5bde0b82392336704094052c1a61478b1
Suraj Saini <suraj88658917@gmail.com>
2026-04-13 15:46:09 +0530

    done Fonts setup

--- App.jsx
+++ App.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>App</Text>
+      <Text style={{fontSize:23 , fontFamily:"Poppins-Bold"}}>This is App2</Text>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1 ,
+     justifyContent:"center",
+     alignItems:"center",
+     backgroundColor:"pink"
+  }
+})
